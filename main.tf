6f5f85b625442d7c98b59e6d68d27425bbf594dd
praveenkurra <praveen.kurra@gmail.com>
2019-10-04 04:51:16 +0200

    added a resource aws_security_group for opening the 8080 port

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
-
+# you are creating a provider
 provider "aws" {
   region = "us-east-1"
 }
+
+#You are creating a resource with an ami and running a bash script.
+#nohub will keep the webserver in running mode
 
 resource "aws_instance" "example" {
   ami           = "ami-0cfee17793b08a293"
@@ -18,5 +21,18 @@ resource "aws_instance" "example" {
   }
 }
 
+#the following resource is important because, you cannot the webserver
+#you created in the above statement without opening the port 8080
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 
